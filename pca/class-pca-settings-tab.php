9a3bae4270e0c1258f8cda4f2f4f7f89be727bf5
TomEijkelenkamp <tom@really-simple-plugins.com>
2022-07-05 15:19:08 +0200

    metadata download field

--- pca/class-pca-settings-tab.php
+++ pca/class-pca-settings-tab.php
@@ -11,6 +11,9 @@ class WP_SAML_Auth_PCA_Settings_Tab {
         add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'add_tab'));
         add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'add_fields'));
         add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'save_tab_fields'));
+
+		add_action("get_field_edit_sp_metadata", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_sp_metadata"), 10, 1);
+		add_action("get_field_view_sp_metadata", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_sp_metadata"), 10, 1);
 	}
 
 	/**
@@ -74,8 +77,21 @@ class WP_SAML_Auth_PCA_Settings_Tab {
 		);
 	}
 
-    public static function save_tab_fields() {
+    public static function save_tab_fields()
+	{
 		error_log(print_r(get_option('wp_saml_auth_settings'), true));
+	}
+
+	public static function get_field_sp_metadata()
+	{
+		$url = add_query_arg(['token' => wp_create_nonce("pca-nonce")], dirname( __FILE__ ) . 'metadata.php?token=');
+
+		?>
+		<label><?php _e("metadata.xml", "pca") ?></label>
+		<div class="form-control-no-input">
+			<a target="_blank" class="btn btn-primary" href="<?php echo $url ?>"><?php _e("Download", "pca") ?></a>
+		</div>
+		<?php
 	}
 
 }
---
+++ pca/metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *  SAML Metadata view
+ */
+
+require_once('../../../wp-load.php');
+require_once 'vendor/autoload.php';
+
+try {
+	$auth = new OneLogin\Saml2\Auth();
+	$settings = $auth->getSettings();
+
+	error_log(print_r($settings, true));
+
+	$metadata = $settings->getSPMetadata();
+	$errors = $settings->validateMetadata($metadata);
+	if (empty($errors)) {
+		header('Content-Type: text/xml');
+		echo $metadata;
+	} else {
+		throw new OneLogin\Saml2\Error(
+			'Invalid SP metadata: ' . implode(', ', $errors),
+			OneLogin\Saml2\Error::METADATA_SP_INVALID
+		);
+	}
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
