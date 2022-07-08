1fedf184670b84ab1d51f6fe3c75e1f8b7884d2c
TomEijkelenkamp <tom@really-simple-plugins.com>
2022-07-08 15:01:04 +0200

    img added to docu

--- pca/documentation.php
+++ pca/documentation.php
@@ -13,8 +13,19 @@ header('Content-Type: text/html');
 	<h1>Single sign on set up documentation</h1>
 	<h2>Step 1.</h2>
 	<p>Do this</p>
+	<img src="img/docu_1_adfs.png" />
 	<h2>Step 2.</h2>
 	<p>Do this</p>
+	<img src="img/docu_2_add_trust.png" />
 	<h2>Step 3.</h2>
+	<p>Do this</p>
+	<img src="img/docu_3_sp_metadata.png" />
+	<h2>Step 4.</h2>
+	<p>Do this</p>
+	<img src="img/docu_4_import_metadata.png" />
+	<h2>Step 5.</h2>
+	<p>Do this</p>
+	<img src="img/docu_5_name_trust.png" />
+	<h2>Step 6.</h2>
 	<p>Do this</p>
 </div>
