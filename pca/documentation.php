d384002c8613c1ddeafb6115052f29b771b6ee81
TomEijkelenkamp <tom@really-simple-plugins.com>
2022-07-12 11:51:01 +0200

    documentation update

--- pca/documentation.php
+++ pca/documentation.php
@@ -27,43 +27,48 @@ header('Content-Type: text/html');
 	</section>
 	<section>
 		<h2>Step 2.</h2>
-		<p>Open ADFS Management</p>
-		<img src="img/2_add_relying_party_trust.png" />
+		<p>Add relying Party Trust</p>
+		<img src="img/2_0_add_relying_party_trust.png" />
+	</section>
+	<section>
+		<h2>Step 2.1.</h2>
+		<p>Select "Claims aware"</p>
+		<img src="img/2_1_welcome.png" />
+	</section>
+	<section>
+		<h2>Step 2.2.</h2>
+		<p>Download the service provider metadata from the privacy insights platform</p>
+		<img src="img/2_2_download_sp_metadata.png" />
+	</section>
+	<section>
+		<h2>Step 2.3.</h2>
+		<p>Import the metadata.xml into the party trust</p>
+		<img src="img/2_3_import_sp_metadata.png.png" />
+	</section>
+	<section>
+		<h2>Step 2.4.</h2>
+		<p>Name the party trust according to your avg register url, subdomain.domain.nl</p>
+		<img src="img/2_4_specify_name.png" />
+	</section>
+	<section>
+		<h2>Step 2.5.</h2>
+		<p>Set access control policy to everyone</p>
+		<img src="img/2_5_access_control_policy.png" />
+	</section>
+	<section>
+		<h2>Step 2.6.</h2>
+		<p>Press next</p>
+		<img src="img/2_6_ready.png" />
+	</section>
+	<section>
+		<h2>Step 2.7.</h2>
+		<p>Press close</p>
+		<img src="img/2_7_finish.png" />
 	</section>
 	<section>
 		<h2>Step 3.</h2>
-		<p>Add relying Party Trust</p>
-		<img src="img/docu_2_add_trust.png" />
-	</section>
-	<section>
-		<h2>Step 4.</h2>
-		<p>Download the service provider metadata from the privacy insights platform</p>
-		<img src="img/docu_3_sp_metadata.png" />
-	</section>
-	<section>
-		<h2>Step 5.</h2>
-		<p>Import the metadata.xml into the party trust</p>
-		<img src="img/docu_4_import_metadata.png" />
-	</section>
-	<section>
-		<h2>Step 6.</h2>
-		<p>Name the party trust according to your avg register url, subdomain.domain.nl</p>
-		<img src="img/docu_5_name_trust.png" />
-	</section>
-	<section>
-		<h2>Step 7.</h2>
-		<p>Set access control policy to everyone</p>
-		<img src="img/docu_6_access_control.png" />
-	</section>
-	<section>
-		<h2>Step 8.</h2>
-		<p>Press next</p>
-		<img src="img/docu_7_trust_ready.png" />
-	</section>
-	<section>
-		<h2>Step 9.</h2>
-		<p>Press close</p>
-		<img src="img/docu_8_trust_finish.png" />
+		<p>Edit Claim Issuance Policy</p>
+		<img src="img/3_0_edit_claim_issuance_policy.png" />
 	</section>
 	<section>
 		<h2>Step 10.</h2>
