7627f81d07b2542cb26929e2177b39aafe4f6618
TomEijkelenkamp <tom@really-simple-plugins.com>
2022-08-19 14:31:38 +0200

    reseller->account['permit_wp_login']

--- pca/class-pca-settings-tab.php
+++ pca/class-pca-settings-tab.php
@@ -10,14 +10,17 @@ class WP_SAML_Auth_PCA_Settings_Tab {
 
         add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'add_tab'));
         add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'add_fields'));
-        add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'save_tab_fields'));
+		add_filter('pca_field_get_value', array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_value_wp_saml_auth"), 10, 4);
+		add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'save_tab_fields'));
 
 		add_action("get_field_edit_sp_metadata", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_sp_metadata"), 10, 1);
 		add_action("get_field_view_sp_metadata", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_sp_metadata"), 10, 1);
 		add_action("get_field_edit_documentation", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_documentation"), 10, 1);
 		add_action("get_field_view_documentation", array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_documentation"), 10, 1);
 
-		add_filter('pca_field_get_value', array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_value_wp_saml_auth"), 10, 4);
+		add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'add_fields_reseller_account'));
+		add_filter('pca_field_get_value', array('WP_SAML_Auth_PCA_Settings_Tab', "get_field_value_wp_saml_auth_reseller_account"), 10, 4);
+        add_action('init', array('WP_SAML_Auth_PCA_Settings_Tab', 'save_reseller_account_tab_fields'));
 	}
 
 
@@ -102,6 +105,35 @@ class WP_SAML_Auth_PCA_Settings_Tab {
 
 
 	/**
+	 * Add fields to tab
+	 */
+    public static function add_fields_reseller_account()
+	{
+		global $pca_settings;
+
+		$pca_settings->meta["access"]["permit_wp_login"] = array(
+			"label" => __("Sta wordpress login toe", "pca"),
+			"fieldtype" => "checkbox",
+			"roles" => array(PCA_ADMINISTRATOR),
+		);
+	}
+
+
+	/**
+	 * Get values for pca saml fields from wp_saml_auth_settings
+	 */
+	public static function get_field_value_wp_saml_auth( $value, $fieldname, $data_item, $datatype )
+	{
+		if ( $datatype === 'access' ) {
+			$wp_saml_auth_settings = get_option('wp_saml_auth_settings');
+			return isset($wp_saml_auth_settings[$fieldname]) ? esc_html($wp_saml_auth_settings[$fieldname]) : '';
+		}
+
+		return $value;
+	}
+
+
+	/**
 	 * Sanitize and save pca POST to wp_saml_auth_settings
 	 */
     public static function save_tab_fields()
@@ -122,16 +154,30 @@ class WP_SAML_Auth_PCA_Settings_Tab {
 
 
 	/**
-	 * Get values for pca saml fields from wp_saml_auth_settings
+	 * Get values for pca saml fields from wp_saml_auth_settings for reseller->account['permit_wp_login']
 	 */
-    public static function get_field_value_wp_saml_auth( $value, $fieldname, $data_item, $datatype )
+	public static function get_field_value_wp_saml_auth_reseller_account( $value, $fieldname, $data_item, $datatype )
 	{
-		if ( $datatype === 'saml' ) {
+		if ( $datatype === 'account' && $fieldname === 'permit_wp_login') {
 			$wp_saml_auth_settings = get_option('wp_saml_auth_settings');
-			return isset($wp_saml_auth_settings[$fieldname]) ? esc_html($wp_saml_auth_settings[$fieldname]) : '';
+			return isset($wp_saml_auth_settings['permit_wp_login']) ? esc_html($wp_saml_auth_settings['permit_wp_login']) : '';
 		}
 
 		return $value;
+	}
+
+
+	/**
+	 * Sanitize and save pca POST to wp_saml_auth_settings for reseller->account['permit_wp_login']
+	 */
+    public static function save_reseller_account_tab_fields()
+	{
+		if ( isset($_POST['save-account']) && isset($_POST["token"]) && wp_verify_nonce($_POST["token"], "pca-nonce") && isset($_POST["list_account_id"]) && isset($_POST["permit_wp_login"]) )
+		{
+			$wp_saml_auth_settings = get_option('wp_saml_auth_settings');
+			$wp_saml_auth_settings['permit_wp_login'] = sanitize_title($_POST['permit_wp_login']);
+			update_option('wp_saml_auth_settings', $wp_saml_auth_settings);
+		}
 	}
 
 
