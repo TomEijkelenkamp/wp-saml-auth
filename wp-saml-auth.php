0272982b824bacb5aea7d1473dea748edf0c32c7
TomEijkelenkamp <tom@really-simple-plugins.com>
2022-08-22 10:08:29 +0200

    also load on reseller

--- wp-saml-auth.php
+++ wp-saml-auth.php
@@ -12,9 +12,14 @@
  * @package Wp_Saml_Auth
  */
 
+
+/**
+ * Only load wp-saml-auth on Enterprise or Reseller
+ */
 global $wpdb;
 $subscription = $wpdb->get_var("select subscription from {$wpdb->base_prefix}pca_account where blog_id = " . get_current_blog_id());
-if ( $subscription != 3 ) {
+$reseller = get_blog_option($blog_id, 'reseller');
+if ( $subscription != 3 || !$reseller) {
 	return;
 }
 
